14e3937df7b08448e62210444710ef0dd6e20895
DeadMakar <ssapientiaa@gmail.com>
2024-02-29 14:58:48 +0200

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; 
-import './index.css';
+import App from './App.jsx';
+// import { persistor, store } from './redux/store.js';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite">
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter basename="/PowerPulseTeamPoject">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+    {/* </PersistGate>
+    </Provider> */}
+  </React.StrictMode>
 );
