35f46b983fc7f517235a5e88a231d50c12c11f79
makswelll <nikolaevic21.maks@gmail.com>
2024-03-08 19:29:42 +0200

    fix

--- src/components/LogOutBtn/LogOutBtn.jsx
+++ src/components/LogOutBtn/LogOutBtn.jsx
@@ -3,16 +3,11 @@ import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/operations';
 import { LogoutIcon, LogoutStyled, SpanText } from './LogOutBtn.styled';
 
-const LogOutBtn = ({ onClick, colorSvg = 'rgba(239, 237, 232, 1)' }) => {
+const LogOutBtn = ({ colorSvg = 'rgba(239, 237, 232, 1)' }) => {
   const dispatch = useDispatch();
 
-  const handleCloseClick = () => {
-    dispatch(logOut());
-    onClick();
-  };
-
   return (
-    <LogoutStyled to="/welcome" onClick={handleCloseClick}>
+    <LogoutStyled to="/welcome" onClick={() => dispatch(logOut())}>
       <SpanText>Logout</SpanText>
       <LogoutIcon $colorSvg={colorSvg}>
         <use href={`${sprite}#icon-log-out-01`} />
