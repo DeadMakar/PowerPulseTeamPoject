9ac4d3c81cb01f9b5711d022c1659c2841921056
makswelll <nikolaevic21.maks@gmail.com>
2024-03-08 22:02:06 +0200

    headerfix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,12 +17,15 @@ import LogOutBtn from '../LogOutBtn/LogOutBtn';
 import UserBar from '../UserBar/UserBar';
 import { MobileMenu } from '../MobileMenu';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState);
   };
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <HeaderContainer>
@@ -30,28 +33,29 @@ const Header = () => {
         <Link to="/">
           <Logo />
         </Link>
+        {isLoggedIn && (
+          <SecondNavWraper>
+            <MediaQuery minWidth={1440}>
+              <NavWraper>
+                <NavigationLink to="/diary">Diary</NavigationLink>
+                <NavigationLink to="/products">Products</NavigationLink>
+                <NavigationLink to="/exercises">Exercises</NavigationLink>
+              </NavWraper>
+            </MediaQuery>
 
-        <SecondNavWraper>
-          <MediaQuery minWidth={1440}>
-            <NavWraper>
-              <NavigationLink to="/diary">Diary</NavigationLink>
-              <NavigationLink to="/products">Products</NavigationLink>
-              <NavigationLink to="/exercises">Exercises</NavigationLink>
-            </NavWraper>
-          </MediaQuery>
-
-          <NavigationLinkItem to="/profile">
-            <UserBar />
-          </NavigationLinkItem>
-          <MediaQuery minWidth={1440}>
-            <LogOutBtn />
-          </MediaQuery>
-          <MediaQuery maxWidth={1439}>
-            <BurgerBtn onClick={toggleMenu}>
-              <use href={`${sprite}#icon-menu`} />
-            </BurgerBtn>
-          </MediaQuery>
-        </SecondNavWraper>
+            <NavigationLinkItem to="/profile">
+              <UserBar />
+            </NavigationLinkItem>
+            <MediaQuery minWidth={1440}>
+              <LogOutBtn />
+            </MediaQuery>
+            <MediaQuery maxWidth={1439}>
+              <BurgerBtn onClick={toggleMenu}>
+                <use href={`${sprite}#icon-menu`} />
+              </BurgerBtn>
+            </MediaQuery>
+          </SecondNavWraper>
+        )}
       </Navigation>
       <MediaQuery maxWidth={1439}>
         {isMenuOpen && <MobileMenu onClick={toggleMenu} />}
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,3 @@
-// import {
-//   StyledDrop,
-//   StyledDropContainer,
-//   StyledH1,
-//   StyledLetter,
-// } from './Loader.styled';
 import PacmanLoader from 'react-spinners/PacmanLoader';
 const Loader = () => {
   return <PacmanLoader color="rgba(255, 130, 0, 1)" />;
