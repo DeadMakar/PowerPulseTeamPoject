9fec1b7f905650a8cdae75b6f6a5150ca12cc56f
DeadMakar <ssapientiaa@gmail.com>
2024-03-09 16:15:52 +0200

    add routes

--- src/pages/ExercisesPage/ExercisesPage.jsx
+++ src/pages/ExercisesPage/ExercisesPage.jsx
@@ -1,152 +1,76 @@
-// import { Suspense, useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
-// import sprite from '../../assets/sprite.svg';
-// import { getError, getIsLoading } from '../../redux/exercises/categoriesSlice';
-// import { ExercisesCategories } from '../../components/ExercisesCategories/ExercisesCategories.styled';
-// import { fetchExercisesCategories } from '../../redux/exercises/operations';
-// import { Container } from '../../styles/GlobalStyles';
-// import {
-//   ContainerExPage,
-//   BackButton,
-//   BackGroundStyle,
-//   MixContainer,
-// } from './ExercisesPage.styled';
-// import { TitlePage } from '../../components/TitlePage';
-// import { Loader } from '../../components/Loader';
-// import { toast } from 'react-toastify';
-
-// const ExercisesPage = () => {
-//   const dispatch = useDispatch();
-//   const isLoading = useSelector(getIsLoading);
-//   const error = useSelector(getError);
-//   const { filter, filterList } = useParams();
-// const navigate = useNavigate();
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const gettingExercisesFilters = async () => {
-//       if (filter === undefined) {
-//         {
-//           toast.info('We are still awating for data', {
-//             theme: 'dark',
-//           });
-//         }
-//       } else {
-//         dispatch(fetchExercisesCategories({ filter: filter }));
-//       }
-//     };
-//     gettingExercisesFilters();
-//   }, [dispatch, filter]);
-//   return (
-// <BackGroundStyle>
-//       <Container>
-//         <MixContainer>
-//           {isLoading ? (
-//             <Loader />
-//           ) : (
-//             <div>
-//               {filter !== undefined &&
-//               location.pathname.endsWith(filter.replace(' ', '%20')) ? null : (
-// {
-//   <BackButton onClick={() => navigate(-1)}>
-//   <svg style={{ fill: 'none' }}>
-//     <use href={sprite + '#icon-arrow-left'} />
-//   </svg>
-//   Back{' '}
-// </BackButton>;
-// }
-//               )}
-//               <ContainerExPage>
-//                 <TitlePage
-//                   title={filterList ? filterList.split(' ')[0] : 'Exercises'}
-//                 />
-//                 <ExercisesCategories />
-//               </ContainerExPage>
-
-//               {isLoading && !error && <Loader />}
-//               <>
-//                 <Suspense>
-//                   <Outlet />
-//                 </Suspense>
-//               </>
-//             </div>
-//           )}
-//         </MixContainer>
-//       </Container>
-//     </BackGroundStyle>
-//   );
-// };
-
-// export default ExercisesPage;
-
-import { useState, Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
+import sprite from '../../assets/sprite.svg';
+import { getError, getIsLoading } from '../../redux/exercises/categoriesSlice';
+import ExercisesCategories from '../../components/ExercisesCategories/ExercisesCategories';
+import { fetchExercisesCategories } from '../../redux/exercises/operations';
+import { Container } from '../../styles/GlobalStyles';
 import {
-  Outlet,
-  // useNavigate
-} from 'react-router-dom';
-import { SectionTemplate } from '../../components/SectionTemplate';
-import { TitlePage } from '../../components/TitlePage';
-import { ChapterTemplate } from '../../components/ChapterTemplate';
-import { AddExerciseSuccess } from '../../components/AddExerciseSuccess';
-import { BasicModalWindow } from '../../components/BasicModalWindow';
-import {
-  ChaptersWrapper,
-  LinkStyled,
-  DesctopWrapper,
+  ContainerExPage,
+  BackButton,
   BackGroundStyle,
-  // BackButton,
+  MixContainer,
 } from './ExercisesPage.styled';
-
-// import sprite from '../../assets/sprite.svg';
+import { TitlePage } from '../../components/TitlePage';
+import { Loader } from '../../components/Loader';
+import { toast } from 'react-toastify';
 
 const ExercisesPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  const { filter, filterList } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  // const navigate = useNavigate();
-
-  const toggleModal = () => {
-    setIsModalOpen((prevState) => !prevState);
-  };
+  useEffect(() => {
+    const gettingExercisesFilters = async () => {
+      if (
+        filter !== undefined &&
+        filterList !== undefined &&
+        filterList.trim() !== ''
+      ) {
+        dispatch(fetchExercisesCategories({ filter: filter }));
+      } else {
+        toast.info('We are still awaiting for data', {
+          theme: 'dark',
+        });
+      }
+    };
+    gettingExercisesFilters();
+  }, [dispatch, filter, filterList]);
 
   return (
     <BackGroundStyle>
-      <SectionTemplate>
-        {/* <BackButton onClick={() => navigate(-1)}>
-          <svg style={{ fill: 'none' }}>
-            <use href={sprite + '#icon-arrow-left'} />
-          </svg>
-          Back{' '}
-        </BackButton> */}
-        <DesctopWrapper>
-          <TitlePage>Exercises</TitlePage>
-          <ChaptersWrapper>
-            <li>
-              <LinkStyled to="body parts">
-                <ChapterTemplate>Body parts</ChapterTemplate>
-              </LinkStyled>
-            </li>
-            <li>
-              <LinkStyled to="muscles">
-                <ChapterTemplate>Muscles</ChapterTemplate>
-              </LinkStyled>
-            </li>
-            <li>
-              <LinkStyled to="equipment">
-                <ChapterTemplate>Equipment</ChapterTemplate>
-              </LinkStyled>
-            </li>
-          </ChaptersWrapper>
-        </DesctopWrapper>
-        <Suspense fallback={<p>Loader</p>}>
-          {isModalOpen && (
-            <BasicModalWindow onClick={toggleModal}>
-              <AddExerciseSuccess onClick={toggleModal} />
-            </BasicModalWindow>
+      <Container>
+        <MixContainer>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div>
+              {filter !== undefined &&
+              location.pathname.endsWith(filter.replace(' ', '%20')) ? null : (
+                <BackButton onClick={() => navigate(-1)}>
+                  <svg style={{ fill: 'none' }}>
+                    <use href={sprite + '#icon-arrow-left'} />
+                  </svg>
+                  Back
+                </BackButton>
+              )}
+              <ContainerExPage>
+                <TitlePage contentText="Exercises" />
+                <ExercisesCategories />
+              </ContainerExPage>
+
+              {isLoading && !error && <Loader />}
+              <Suspense fallback={<Loader />}>
+                <Outlet />
+              </Suspense>
+            </div>
           )}
-          <Outlet />
-        </Suspense>
-      </SectionTemplate>
+        </MixContainer>
+      </Container>
     </BackGroundStyle>
   );
 };
