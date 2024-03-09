185277cf1311e6ad1bec1b0d05891278f104c045
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2024-03-09 17:33:47 +0100

    pj-08
    я добавив файл з кодом

---
+++ src/pages/ExercisesPage/Exercises.styled.js
@@ -0,0 +1,48 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const ChaptersWrapper = styled.ul`
+  display: flex;
+  gap: 28px;
+  margin-top: 20px;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+  }
+  @media screen and (min-width: 1440px) {
+    justify-content: end;
+  }
+`;
+
+export const DesctopWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+export const LinkStyled = styled(NavLink)`
+  position: relative;
+  display: block;
+  color: rgba(239, 237, 232, 0.4);
+
+  &.active {
+    color: var(--color-text);
+
+    &::before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      bottom: -8px;
+      width: 100%;
+      height: 4px;
+      background-color: var(--color-main-one);
+      border-radius: 2px;
+    }
+  }
+`;
