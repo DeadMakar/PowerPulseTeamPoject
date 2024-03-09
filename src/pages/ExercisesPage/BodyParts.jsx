6ace7ac8319959fc797d653a0918b6c4cef5b341
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2024-03-09 13:21:27 +0100

    pj-07

--- src/pages/ExercisesPage/BodyParts.jsx
+++ src/pages/ExercisesPage/BodyParts.jsx
@@ -1,79 +1,55 @@
-import { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useLocation, useNavigate, useParams } from 'react-router-dom';
-import sprite from '../../assets/sprite.svg';
-import { getError, getIsLoading } from '../../redux/exercises/categoriesSlice';
-import { ExercisesCategories } from '../../components/ExercisesCategories/ExercisesCategories.styled';
-import { fetchExercisesCategories } from '../../redux/exercises/operations';
-import { Container } from '../../styles/GlobalStyles';
-import {
-  ContainerExPage,
-  BackButton,
-  BackGroundStyle,
-  MixContainer,
-} from './ExercisesPage.styled';
+import { useState, Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { SectionTemplate } from '../../components/SectionTemplate';
 import { TitlePage } from '../../components/TitlePage';
-import { Loader } from '../../components/Loader';
-import { toast } from 'react-toastify';
+import { ChapterTemplate } from '../../components/ChapterTemplate';
+import { AddExerciseSuccess } from '../../components/AddExerciseSuccess/index';
+import { BasicModalWindow } from '../../components/BasicModalWindow';
+import {
+  ChaptersWrapper,
+  LinkStyled,
+  DesctopWrapper,
+} from './BodyParts.styled';
 
-const ExercisesPage = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(getIsLoading);
-  const error = useSelector(getError);
-  const { filter, filterList } = useParams();
-  const navigate = useNavigate();
-  const location = useLocation();
+const Exercises = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    const gettingExercisesFilters = async () => {
-      if (filter === undefined) {
-        {
-          toast.info('We are still awating for data', {
-            theme: 'dark',
-          });
-        }
-      } else {
-        dispatch(fetchExercisesCategories({ filter: filter }));
-      }
-    };
-    gettingExercisesFilters();
-  }, [dispatch, filter]);
+  const toggleModal = () => {
+    setIsModalOpen((prevState) => !prevState);
+  };
+
   return (
-    <BackGroundStyle>
-      <Container>
-        <MixContainer>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <div>
-              {filter !== undefined &&
-              location.pathname.endsWith(filter.replace(' ', '%20')) ? null : (
-                <BackButton onClick={() => navigate(-1)}>
-                  <svg style={{ fill: 'none' }}>
-                    <use href={sprite + '#icon-arrow-left'} />
-                  </svg>
-                  Back{' '}
-                </BackButton>
-              )}
-              <ContainerExPage>
-                <TitlePage
-                  title={filterList ? filterList.split(' ')[0] : 'Exercises'}
-                />
-                <ExercisesCategories />
-              </ContainerExPage>
-
-              {isLoading && !error && <Loader />}
-              <>
-                <Suspense>
-                  <Outlet />
-                </Suspense>
-              </>
-            </div>
-          )}
-        </MixContainer>
-      </Container>
-    </BackGroundStyle>
+    <SectionTemplate>
+      <DesctopWrapper>
+        <TitlePage>Exercises</TitlePage>
+        <ChaptersWrapper>
+          <li>
+            <LinkStyled to="body parts">
+              <ChapterTemplate>Body parts</ChapterTemplate>
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled to="muscles">
+              <ChapterTemplate>Muscles</ChapterTemplate>
+            </LinkStyled>
+          </li>
+          <li>
+            <LinkStyled to="equipment">
+              <ChapterTemplate>Equipment</ChapterTemplate>
+            </LinkStyled>
+          </li>
+        </ChaptersWrapper>
+      </DesctopWrapper>
+      <Suspense fallback={<p>Loader</p>}>
+        {isModalOpen && (
+          <BasicModalWindow onClick={toggleModal}>
+            <AddExerciseSuccess onClick={toggleModal} />
+          </BasicModalWindow>
+        )}
+        <Outlet />
+      </Suspense>
+    </SectionTemplate>
   );
 };
 
-export default ExercisesPage;
+export default Exercises;
--- src/pages/ExercisesPage/BodyParts.styled.js
+++ src/pages/ExercisesPage/BodyParts.styled.js
@@ -1,116 +1,48 @@
-import styled from '@emotion/styled';
-import { globalColor } from '../../styles/root';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-import bgDesktop1 from '../../assets/images/exercises/trainingGym-1.jpg';
-import bgDesktop2 from '../../assets/images/exercises/trainingGym-1@2x.jpg';
-
-export const ContainerExPage = styled.div`
-  /* border: 2px solid red;
-   position: relative; */
+export const ChaptersWrapper = styled.ul`
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
+  gap: 28px;
   margin-top: 20px;
-  margin-bottom: 40px;
 
-  @media (min-width: 768px) {
+  @media screen and (min-width: 768px) {
+    margin-top: 0;
+  }
+  @media screen and (min-width: 1440px) {
+    justify-content: end;
+  }
+`;
+
+export const DesctopWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    display: flex;
     flex-direction: row;
-
-    margin-top: 78px;
-    margin-bottom: 70px;
-  }
-
-  @media (max-width: 374px) {
+    justify-content: space-between;
+    align-items: center;
   }
 `;
-export const Block = styled.div`
-  text-align: center;
-`;
-
-export const ExerPageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  height: 100vh;
-
-  @media (min-width: 768px) {
-    gap: 32px;
-  }
-`;
-
-export const MixContainer = styled.div`
+export const LinkStyled = styled(NavLink)`
   position: relative;
-  width: 100%;
-  height: 100%;
-  border: 1px solid transparent;
-  /* border: 2px solid red; */
-`;
+  display: block;
+  color: rgba(239, 237, 232, 0.4);
 
-export const BackButton = styled.a`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  top: 6px;
-  gap: 8px;
-  color: ${globalColor.colorInput};
-  font-size: 10px;
-  font-weight: 400;
-  line-height: 1.167;
-  transition: color 0.3s ease;
+  &.active {
+    color: var(--color-text);
 
-  @media (min-width: 768px) {
-    top: 32px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 1.167;
-  }
-
-  > svg {
-    stroke: ${globalColor.colorInput};
-    width: 10px;
-    height: 10px;
-    transition: stroke 0.3s ease;
-    @media (min-width: 768px) {
-      width: 16px;
-      height: 16px;
+    &::before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      bottom: -8px;
+      width: 100%;
+      height: 4px;
+      background-color: var(--color-main-one);
+      border-radius: 2px;
     }
-  }
-
-  &:hover {
-    color: ${globalColor.colorOrange1};
-    cursor: pointer;
-
-    > svg {
-      stroke: ${globalColor.colorOrange1};
-      width: 16px;
-      height: 16px;
-    }
-  }
-`;
-
-export const BackGroundStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 40px;
-  height: 100vh;
-
-  @media (min-width: 768px) {
-    gap: 32px;
-  }
-  @media (min-width: 1440px) {
-    background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%),
-      url(${bgDesktop1});
-    background-repeat: no-repeat;
-    background-position: right bottom;
-    background-size: contain;
-  }
-
-  @media screen and (min-device-pixel-ratio: 2) and (min-width: 1440px),
-    screen and (-webkit-min-device-pixel-ratio: 2) and (min-width: 1440px),
-    screen and (min-resolution: 192dpi) and (min-width: 1440px),
-    screen and (min-resolution: 2dppx) and (min-width: 1440px) {
-    background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%),
-      url(${bgDesktop2});
   }
 `;
