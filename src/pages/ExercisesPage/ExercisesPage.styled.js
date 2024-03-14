985f14c73755673c87585295fba685420ce9feaf
makswelll <nikolaevic21.maks@gmail.com>
2024-03-14 17:45:27 +0200

    fixstyled

--- src/pages/ExercisesPage/ExercisesPage.styled.js
+++ src/pages/ExercisesPage/ExercisesPage.styled.js
@@ -87,7 +87,8 @@ export const BackGroundStyle = styled.div`
     gap: 32px;
   }
   @media (min-width: 1440px) {
-    background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%);
+    background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%),
+      url(${bgDesktop1});
     background-repeat: no-repeat;
     background-position: right bottom;
     background-size: contain;
@@ -101,111 +102,3 @@ export const BackGroundStyle = styled.div`
       url(${bgDesktop2});
   }
 `;
-
-// export const ContainerExPage = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-between;
-
-//   margin-top: 20px;
-//   margin-bottom: 40px;
-
-//   @media (min-width: 768px) {
-//     flex-direction: row;
-
-//     margin-top: 78px;
-//     margin-bottom: 70px;
-//   }
-
-//   @media (max-width: 374px) {
-//   }
-// `;
-// export const Block = styled.div`
-//   text-align: center;
-// `;
-
-// export const ExerPageWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 40px;
-//   height: 100vh;
-
-//   @media (min-width: 768px) {
-//     gap: 32px;
-//   }
-// `;
-
-// export const MixContainer = styled.div`
-//   position: relative;
-//   width: 100%;
-//   height: 100%;
-//   border: 1px solid transparent;
-// `;
-
-// export const BackButton = styled.a`
-//   position: absolute;
-//   display: flex;
-//   align-items: center;
-//   top: 6px;
-//   gap: 8px;
-//   color: ${globalColor.colorInput};
-//   font-size: 10px;
-//   font-weight: 400;
-//   line-height: 1.167;
-//   transition: color 0.3s ease;
-
-//   @media (min-width: 768px) {
-//     top: 32px;
-//     font-size: 16px;
-//     font-weight: 400;
-//     line-height: 1.167;
-//   }
-
-//   > svg {
-//     stroke: ${globalColor.colorInput};
-//     width: 10px;
-//     height: 10px;
-//     transition: stroke 0.3s ease;
-//     @media (min-width: 768px) {
-//       width: 16px;
-//       height: 16px;
-//     }
-//   }
-
-//   &:hover {
-//     color: ${globalColor.colorOrange1};
-//     cursor: pointer;
-
-//     > svg {
-//       stroke: ${globalColor.colorOrange1};
-//       width: 16px;
-//       height: 16px;
-//     }
-//   }
-// `;
-
-// export const BackGroundStyle = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 40px;
-//   height: 100vh;
-
-//   @media (min-width: 768px) {
-//     gap: 32px;
-//   }
-//   @media (min-width: 1440px) {
-//     background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%),
-//       url(${bgDesktop1});
-//     background-repeat: no-repeat;
-//     background-position: right bottom;
-//     background-size: contain;
-//   }
-
-//   @media screen and (min-device-pixel-ratio: 2) and (min-width: 1440px),
-//     screen and (-webkit-min-device-pixel-ratio: 2) and (min-width: 1440px),
-//     screen and (min-resolution: 192dpi) and (min-width: 1440px),
-//     screen and (min-resolution: 2dppx) and (min-width: 1440px) {
-//     background-image: linear-gradient(89deg, #040404 0%, rgba(4, 4, 4, 0) 100%),
-//       url(${bgDesktop2});
-//   }
-// `;
